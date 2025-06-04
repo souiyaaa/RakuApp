29e544d95fb93bccbb333379708d13010da8df3c
souiyaaa <153163259+souiyaaa@users.noreply.github.com>
2025-06-04 15:54:35 +0700

    updated early watchConnectivity

--- RakuApp/RakuAppApp.swift
+++ RakuApp/RakuAppApp.swift
@@ -11,30 +11,42 @@ import Firebase
 
 @main
 struct RakuAppApp: App {
-    @StateObject private var authVM = AuthViewModel(userViewModel: UserViewModel())
-    @StateObject private var userVM = UserViewModel()
+    @StateObject private var userVM: UserViewModel
+    @StateObject private var authVM: AuthViewModel
     @StateObject private var matchVM = MatchViewModel()
-    @StateObject private var GameVM = GameViewModel(userViewModel: UserViewModel())
+    @StateObject private var gameVM: GameViewModel
     @StateObject private var gripVM = GripViewModel()
-    @StateObject private var activityVM = ActivityViewModel(userViewModel: UserViewModel())
-    
-    init(){
+    @StateObject private var activityVM: ActivityViewModel
+
+    init() {
         FirebaseApp.configure()
-        
-    #if DEBUG
+
+        #if DEBUG
         let providerFactory = AppCheckDebugProviderFactory()
         AppCheck.setAppCheckProviderFactory(providerFactory)
-    #endif
-        
+        #endif
+
+        // Dependency injection
+        let userVM = UserViewModel()
+        _userVM = StateObject(wrappedValue: userVM)
+
+        let authVM = AuthViewModel(userViewModel: userVM)
+        _authVM = StateObject(wrappedValue: authVM)
+
+        let gameVM = GameViewModel(userViewModel: userVM)
+        _gameVM = StateObject(wrappedValue: gameVM)
+
+        let activityVM = ActivityViewModel(authViewModel: authVM)
+        _activityVM = StateObject(wrappedValue: activityVM)
     }
-    
+
     var body: some Scene {
         WindowGroup {
             MainView()
                 .environmentObject(authVM)
                 .environmentObject(userVM)
                 .environmentObject(matchVM)
-                .environmentObject(GameVM)
+                .environmentObject(gameVM)
                 .environmentObject(gripVM)
                 .environmentObject(activityVM)
         }
